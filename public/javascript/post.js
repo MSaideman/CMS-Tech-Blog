971522db456bfb1beaa1923b10fb128ccbc0852a
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-09-13 15:25:09 -0400

    added create post js and view

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -1,1 +1,24 @@
 // function to create post
+
+// function to login to page
+
+async function postForm(event) {
+    event.preventDefault();
+    const title = document.querySelector("#title").value
+    const content = document.querySelector("#content").value
+
+        const response = await fetch('/api/users/login', {
+            method:"post",
+            body: JSON.stringify({
+                title,
+                content
+            })
+        })
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+
+document.querySelector('.postBody').addEventListener('submit', loginForm);
