22cacbf912ad6ae57ad7f0f8a20d35d6d400c401
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-09-13 15:18:27 -0400

    added signup functionality

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,1 +1,24 @@
 // function to sign up
+
+async function signUpForm(event) {
+    event.preventDefault();
+    const email = document.querySelector("#email").value.trim();
+    const password = document.querySelector("#password").value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method:"post",
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.signUpBody').addEventListener('submit', signUpForm);
