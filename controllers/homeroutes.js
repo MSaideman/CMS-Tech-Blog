cff9b2634beb6e7b9bb65caffb96149ba93821db
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-09-15 20:09:41 -0400

    update handlebars

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -53,7 +53,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
     const postData = await Post.findAll({
       where: {
         user_id: req.session.user_id,
-      },
+      }
     });
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render('dashboard', {
@@ -99,7 +99,36 @@ router.get('/edit/:id', withAuth, async (req, res) => {
 
 router.get('/post/:id', async (req, res) => {
   try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      attributes: [
+        'id',
+        'title',
+        'date_created',
+        'content', 
+        'user_id'
+      ],
+      include: [
+        {
+          model: Comments,
+          attributes: ["id", "content", "post_id", "user_id", "date_created"],
+          include: {
+            model: User,
+            attributes: ['name']
+          }
+        },
+        {
+          model:User,
+          attributes: ['name']
+        }
+      ]
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts)
     res.render('singlepost', {
+      posts,
       loggedIn: req.session.loggedIn
     });
   } catch (err) {
@@ -108,17 +137,4 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-// update this function to show comments
-router.get('/comments/', async (req, res) => {
-  try {
-    const post = req.get({plain: true})
-    res.render('singlepost', {
-      post,
-      loggedIn: req.session.loggedIn
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
 module.exports = router;
--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -6,7 +6,6 @@ async function commentForm(event) {
     const post_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-  
     if (content) {
         const response = await fetch('/api/comments', {
           method: 'POST',
@@ -14,11 +13,11 @@ async function commentForm(event) {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            post_id:post_id,
+            post_id: post_id,
             content: content
           }),
         });
-      
+        console.log(content);
         if (response.ok) {
           document.location.reload();
         } else {
@@ -26,5 +25,4 @@ async function commentForm(event) {
         }
       }
 }
-
 document.querySelector('.comment-form').addEventListener('submit', commentForm);
