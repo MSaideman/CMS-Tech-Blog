e224ba279e9aa3a73e6a2c61da6218c16492d721
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-09-23 19:20:54 -0600

    added timer

--- server.js
+++ server.js
@@ -27,6 +27,15 @@ const hbs = exphbs.create({ helpers });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+  secret: 'secret',
+  name: 'mvc',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+      expires: 120000
+  }
+  }));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
